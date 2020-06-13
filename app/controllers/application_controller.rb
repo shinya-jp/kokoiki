9cee48986cea43834e8c535ebd26babc2b79e166
shinya-jp <shinya0733@gmail.com>
2020-06-13 08:53:36 +0000

    ログイン完成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,11 @@
 class ApplicationController < ActionController::Base
-	before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
-private
-def configure_permitted_parameters
-	devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :gender, :address, :birthday, :email])
-end
+  private
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [
+      :nickname, :gender, :address, :birthday, :email,
+    ])
+  end
 end
