c746e3c8432f98c589d2ff9767e5aa5a84f937e2
shinya-jp <shinya0733@gmail.com>
2020-06-16 14:29:02 +0900

    画像プレビュー修正

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,19 +18,19 @@
 //= require bootstrap-sprockets
 //= require_tree .
 
-$(function() {
-  $(document).on('turbolinks:load',() => {
-    function readURL(input) {
-        if (input.files && input.files[0]) {
-        var reader = new FileReader();
-        reader.onload = function (e) {
-    $('#preview').attr('src', e.target.result);
-        }
-        reader.readAsDataURL(input.files[0]);
-        }
-    }
-    $("#profile_img").change(function(){
-        readURL(this);
-    });
-  });
-});
+// $(function() {
+//   $(document).on('turbolinks:load',() => {
+//     function readURL(input) {
+//         if (input.files && input.files[0]) {
+//         var reader = new FileReader();
+//         reader.onload = function (e) {
+//     $('#preview').attr('src', e.target.result);
+//         }
+//         reader.readAsDataURL(input.files[0]);
+//         }
+//     }
+//     $("#profile_img").change(function(){
+//         readURL(this);
+//     });
+//   });
+// });
