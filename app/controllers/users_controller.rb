5e84e5973bc72d9653cc9d0fa82b701219ec940a
shinya-jp <shinya0733@gmail.com>
2020-06-17 11:56:02 +0000

    フォロー完成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,6 +21,16 @@ class UsersController < ApplicationController
     redirect_to root_path
   end
 
+  def following
+    @user = User.find(params[:id])
+    @users = @user.following
+  end
+
+  def followers
+    @user = User.find(params[:id])
+    @users = @user.followers
+  end
+
   def quit
     @user = User.find(params[:id])
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 Rails.application.routes.draw do
-
 # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'home#top'
   get 'home/about'
   devise_for :users
-  resources :users, only: [:show, :edit, :update, :destroy]
+  resources :users, only: [:show, :edit, :update, :destroy] do
+    member do
+      get :following, :followers
+    end
+    resources :relationships, only: [:create, :destroy]
+  end
   get 'users/:id/quit' => 'users#quit', as:'quit'
-
   resources :places, only: [:new, :create, :show] do
     resources :reviews
     get 'reviews/confirm' => 'reviews#confirm', as: 'review_confirm'
