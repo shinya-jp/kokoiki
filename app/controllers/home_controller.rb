065c422262fe1af8387d603052661913e3e5da4d
shinya-jp <shinya0733@gmail.com>
2020-07-02 15:33:40 +0900

    コメント追加

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,13 +2,15 @@ class HomeController < ApplicationController
   def top
     @user = current_user
     @q = Place.ransack(params[:q])
+    # 遊びジャンルのレビュー
     places_play = Place.where(genre: 1).each do |place_play|
-      # Placeのレビューの平均点を取得
+      # averageカラムを使用　Placeのレビューの平均点を取得
       place_play.average = place_play.average_review_rate
     end
     # 平均点が高い順に表示
     @places_play = places_play.sort_by { |place_play| place_play.average.to_i }.reverse
 
+    # 食事ジャンルのレビュー
     places_meal = Place.where(genre: 2).each do |place_meal|
       place_meal.average = place_meal.average_review_rate
     end
