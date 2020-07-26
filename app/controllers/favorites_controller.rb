f8964ef94a2c1b349b8f6e7f580d7d5f8556b5b6
shinya-jp <shinya0733@gmail.com>
2020-07-26 21:41:34 +0900

    いいねajax追加

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,15 +1,13 @@
 class FavoritesController < ApplicationController
   def create
     @review = Review.find(params[:review_id])
-    favorite = current_user.favorites.new(review_id: @review)
-    favorite.save
-    return
+    favorite = current_user.favorites.new(review_id: @review.id)
+    favorite.save!
   end
 
   def destroy
     @review = Review.find(params[:review_id])
     favorite = current_user.favorites.find_by(review_id: @review)
     favorite.destroy
-    return
   end
 end
