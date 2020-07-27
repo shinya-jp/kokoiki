179d64a7ca02b5dec6ab0f42f8e2e45b59a9eead
shinya-jp <shinya0733@gmail.com>
2020-07-27 20:09:01 +0900

    投稿画像保存

--- config/deploy.rb
+++ config/deploy.rb
@@ -41,5 +41,5 @@ set :repo_url, "git@github.com:shinya-jp/kokoiki.git"
 set :deploy_to, "/home/ec2-user/kokoiki"
 set :rbenv_ruby, '2.5.7'
 set :linked_files, %w{config/master.key .env}
-append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
+append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public"
 
